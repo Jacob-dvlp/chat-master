47fc38db6c6073f9adddf9f0f4e69a75d89c10ca
Jacob Larte <robsonpereirawill@gmail.com>
2023-04-04 19:48:29 +0100

    add getx from this project

--- lib/presenter/chat_gpt/chat_gpt_controller.dart
+++ lib/presenter/chat_gpt/chat_gpt_controller.dart
@@ -1,3 +1,110 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ChatGptController extends GetxController {}
+import '../../core/app_enum.dart';
+import '../../src/model/model_chat.dart';
+import '../../src/repository/repository_chat.dart';
+
+class ChatGptController extends GetxController {
+  final input = TextEditingController();
+  final repository = RepositoryChat(Dio());
+  final List<ModelChat> msg = [];
+  final scrollController = ScrollController();
+  int duration = 200;
+  bool isResponse = false;
+  bool textScanner = false;
+  String scannerText = "";
+  XFile? img;
+
+  scrollDown() {
+    Future.delayed(
+      Duration(milliseconds: duration),
+      () {
+        scrollController.animateTo(scrollController.position.maxScrollExtent,
+            duration: Duration(milliseconds: duration),
+            curve: Curves.easeInOut);
+      },
+    );
+  }
+
+  progrssIndicator(bool value) {
+    isResponse = value;
+    update();
+  }
+
+  progrssIndicatorgetrecognisedText(bool value) {
+    textScanner = value;
+    update();
+  }
+
+  Future sendMsg({required String prompt}) async {
+    progrssIndicator(true);
+    String responseMsg = await repository.getResponse(msg: prompt);
+
+    msg.add(
+      ModelChat(
+        message: responseMsg.trim(),
+        messageFrom: MessageFrom.bot,
+      ),
+    );
+    progrssIndicator(false);
+    scrollDown();
+    update();
+  }
+
+  Future getRecognisedText(XFile img) async {
+    progrssIndicatorgetrecognisedText(true);
+    final inputImage = InputImage.fromFilePath(img.path);
+    final textDetector = GoogleMlKit.vision.textRecognizer();
+    RecognizedText recognizedText = await textDetector.processImage(inputImage);
+    await textDetector.close();
+    scannerText = "";
+    for (TextBlock textBlock in recognizedText.blocks) {
+      for (TextLine line in textBlock.lines) {
+        scannerText = "$scannerText${line.text}\n";
+      }
+    }
+    progrssIndicatorgetrecognisedText(false);
+    await sendMsg(prompt: scannerText);
+    update();
+  }
+
+  Future camera() async {
+    try {
+      final image =
+          await ImagePicker.platform.getImage(source: ImageSource.camera);
+      if (image != null) {
+        img = image;
+        getRecognisedText(img!);
+        update();
+      } else {
+        //  Get.snackbar("Erro ao fazer a leitura de Imagem",);
+      }
+    } catch (e) {
+      textScanner = false;
+    }
+  }
+
+  Future getImage() async {
+    try {
+      final image =
+          await ImagePicker.platform.getImage(source: ImageSource.gallery);
+      if (image != null) {
+        img = image;
+        getRecognisedText(img!);
+        update();
+      } else {
+        // ScaffoldMessenger.of(context).showSnackBar(
+        //   const SnackBar(
+        //     content: Text("Erro ao fazer a leitura de Imagem"),
+        //   ),
+        // );
+      }
+    } catch (e) {
+      textScanner = false;
+    }
+  }
+}
